6ad20e8c0735a2ee8f71767a6fe46135b1af865b
ZXZ1126 <xuezhi@adidesigns.asia>
2022-06-21 15:23:54 +0800

    add mew files

---
+++ BasicCameraPrac01.py
@@ -0,0 +1,10 @@
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024,768)
+    camera.start_preview()
+    #time delay for camera to start
+    time.sleep(2)
+    camera.capture("foo.jpg")
+    

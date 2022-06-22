e24823b591ab3b34e72906651948b00e49fe20d7
ZXZ1126 <xuezhi@adidesigns.asia>
2022-06-22 14:54:58 +0800

    new changes

--- BasicCameraPrac01.py
+++ BasicCameraPrac01.py
@@ -5,6 +5,6 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1024,768)
     camera.start_preview()
     #time delay for camera to start
-    time.sleep(2)
-    camera.capture("foo.jpg")
+    time.sleep(20)
+    camera.capture("foo02.jpg")
     
--- BasicCameraPrac02.py
+++ BasicCameraPrac02.py
@@ -4,7 +4,7 @@ import picamera
 
 import picamera.array
 
-import CV2
+import cv2 as CV2
 
 
 
@@ -12,19 +12,25 @@ with picamera.PiCamera() as camera:
 
         camera.start_preview()
 
-        time.sleep(2)
+        time.sleep(20)
 
         with picamera.array.PiRGBArray(camera) as stream:
 
-        camera.capture(stream, format='rgb')#format类型：bgr\rgb\h264
+            camera.capture(stream, format='rgb')#format类型：bgr\rgb\h264
 
         # 此时就可以获取到bgr的数据流了
 
-        image = stream.array
-
-import matplotlib.pyplot as plt
+            image = stream.array
 
 
-image_resize = CV2.resize(image,(320, 240)) #opencv改变分辨率
 
-plt.imshow(image_resize)
+#image_resize = CV2.resize(image,(1920, 1088)) #opencv改变分辨率
+image_resize = image
+
+CV2.imshow('IMAGE',image_resize)
+
+CV2.imwrite('output2.png',image_resize)
+CV2.imwrite('output2.jpg',image_resize)
+CV2.waitKey(0)
+
+CV2.destroyAllWindows()
